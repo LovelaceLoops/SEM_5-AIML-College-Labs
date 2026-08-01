a4fdc9ca99e314efc0c6b6ebc58c3251c73f5537
LovelaceLoops <mrunalspatil@outlook.com>
2026-08-01 20:24:49 +0530

    Images uploaded

--- Sem_V/SMLD/Data_Processing/Exp_02.py
+++ Sem_V/SMLD/Data_Processing/Exp_02.py
@@ -6,21 +6,21 @@ print(data)
 print(data.columns)
 
 #Find the Total missing values in the Age column
-print(data["Age"].isnull().sum())
+print(f"Total missing values = {data["Age"].isnull().sum()}")
 
 #Fill the missing rows with Mean of the column
 data["Age"] = data["Age"].fillna(data["Age"].mean())
-print(data["Age"].isnull().sum())
+print(f"New number of missing values = {data["Age"].isnull().sum()}")
 
 #Perfome scalling on the age column 
 scalar = StandardScaler()
 data["Age_Scaled"] = scalar.fit_transform(data[["Age"]])
-print(data["Age_Scaled"])
+print(f"Scaled age = {data["Age_Scaled"]}")
 
 #Perform encoding on Sex column
 le = LabelEncoder()
 data["Sex_Encoded"] = le.fit_transform(data[["Sex"]])
-# print(data["Sex_Encoded"])
+print(f"Encoded gender = {data["Sex_Encoded"]}")
 
 #Outlier detection
 Q1 = data['Age'].quantile(0.25)
@@ -31,4 +31,4 @@ lower_bound = Q1 - 1.5 * IQR
 upper_bound = Q3 + 1.5 * IQR
 
 outliers = data[(data['Age'] < lower_bound) | (data['Age'] > upper_bound)]
-print(outliers["Age"])
+print(f"Age outliers = {outliers["Age"]}")
